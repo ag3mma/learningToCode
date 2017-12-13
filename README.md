1ce6474b1c5dc9363e5be1c35afaf267720e6e69
Andrea Gemma <andrea.gemma@hotmail.it>
2017-12-13 19:17:06 +0100

    Messing with VS Code and Vim mode

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # learningToCode
+## That's cool!!
--- jsAlgo.js
+++ jsAlgo.js
@@ -12,5 +12,10 @@ function harmlessRansomNote(noteText, magazineText) {
   var noteArr = noteText.split(' ');
   var magazineArr = magazineText.split(' ');
   var magazineObj = {};
-    
+ 
+  magazineArr.forEach(funtion(word){
+	//create the magazine object and populate it by the occurrences of the words  
+  	if(!magazineObj[word]) magazineObj[word] = 0;
+	magazineObj[word]++;
+  });	
 }
